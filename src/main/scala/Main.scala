c7bc51400547dafdddeab204b47e14076737660e
Lazaros Gogos <lazarosg@csd.auth.gr>
2023-12-19 20:43:19 +0200

    Data cleanup and scaling is complete

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,8 +5,40 @@ object Main {
     val lines = src.getLines()
 
     val validPoints = lines.flatMap(parsePoint).toList
+    val (minX, minY, maxX, maxY) = findMinMaxValues(validPoints)
 
-    println("length:" + validPoints.length)
+    var scaledPoints = List[(Double, Double)]()
+    validPoints.foreach(p => {
+      var x = p._1
+      var y = p._2
+      x = scaleValue(x, minX, maxX)
+      y = scaleValue(y, minY, maxY)
+      val newP = (x,y)
+      scaledPoints = scaledPoints :+ newP
+    })
+
+//    scaledPoints.foreach(println)
+  }
+
+  def scaleValue(value: Double, min: Double, max: Double): Double = {
+    (value - min) / (max - min)
+  }
+
+  def findMinMaxValues(points: List[(Double, Double)]): (Double, Double, Double, Double) = {
+    var minX = Double.MaxValue
+    var minY = Double.MaxValue
+    var maxX = Double.MinValue
+    var maxY = Double.MinValue
+    points.foreach(point => {
+      val x = point._1
+      val y = point._2
+      // update min and max values
+      minX = math.min(minX, x)
+      minY = math.min(minY, y)
+      maxX = math.max(maxX, x)
+      maxY = math.max(maxY, y)
+    })
+    (minX, minY, maxX, maxY)
   }
 
   def parsePoint(line: String): Option[(Double, Double)] = {
@@ -18,18 +50,17 @@ object Main {
       try {
         val x = values(0).trim.toDouble
         val y = values(1).trim.toDouble
-        Some((x,y))
+        Some((x, y))
       } catch {
         case e: NumberFormatException =>
           // Ignore lines that cannot be parsed as doubles
-          println("exception:"+line)
+//          println("exception:" + line)
           None
       }
     } else {
       // Ignore lines that do not have both x and y values
-      println("Length not 2:"+line)
+//      println("Length not 2:" + line)
       None
     }
-
   }
 }
