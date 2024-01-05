125880cc21dd709cd43705efa21a987bb66d953e
Vasilis Papastergios <papastva@csd.auth.gr>
2024-01-05 13:45:46 +0200

    Added JavaDoc

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,11 @@ import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 
 import java.io.PrintWriter
 
+/**
+ * Data WareHouses & Data Mining Course, AUTh
+ * Semester programming assignment (Winter 2023-2024)
+ * @author Vasileios Papastergios (ID: 3651), Lazaros Gogos (ID: 3877)
+ */
 object Main {
   private var xMin: Double = 0.0
   private var xMax: Double = 1.0
@@ -45,6 +50,13 @@ object Main {
     sc.stop(0)
   }
 
+  /**
+   * A utility function that initializes a SparkSession instance with defined parameters.
+   *
+   * @param cores   the number of cores to use as String. If '*', all available cores are used.
+   * @param appName the name of the application.
+   * @return a SparkSession instance with the defined parameters.
+   */
   private def initializeSparkSession(cores: String, appName: String): SparkSession = {
     SparkSession.builder()
       .master(s"local[$cores]")
@@ -52,6 +64,14 @@ object Main {
       .getOrCreate()
   }
 
+  /**
+   * A utility function that extracts the input file name from command line arguments array, if specified.
+   * In case there are no command line arguments, a default file name is used (data.csv).
+   * Corresponds to assignment TASK 1.
+   *
+   * @param args the command line arguments of function main.
+   * @return the file name as String (if existent in arguments), else a default file name "data.csv".
+   */
   private def getInputFileName(args: Array[String]): String = {
     val defaultFileName = "data.csv"
     if (args.length == 0) {
@@ -60,13 +80,42 @@ object Main {
     args(0)
   }
 
-
-  //    ASSIGNMENT TASK 1
+  /**
+   * A utility function that is responsible for reading the input data file and constructing an RDD with the data.
+   * Corresponds to assignment TASK 1.
+   *
+   * @param sc   the SparkContext object.
+   * @param args the command line arguments array of function main.
+   * @return an RDD with the raw input data.
+   */
   private def readInputDataFile(sc: SparkContext, args: Array[String]): RDD[String] = {
     val inputFile = getInputFileName(args)
     sc.textFile(inputFile)
   }
 
+
+  /**
+   * A utility function that preforms data cleaning on an RDD[String] containing "x,y" coordinates.
+   * The function removes lines with missing values, splits the "x,y" String per row to (Double, Double)
+   * coordinates and constructs an RDD with the clean data.
+   * Corresponds to assignment TASK 2.
+   *
+   * @param rawData the RDD[String] to clean.
+   * @return an RDD[(Double, Double)] containing the clean data.
+   */
+  private def performDataCleaning(rawData: RDD[String]): RDD[(Double, Double)] = {
+    val cleanData = filterOutLinesWithMissingValues(rawData)
+    splitLinesToDoubleCoordinates(cleanData)
+  }
+
+  /**
+   * A utility function that, given an RDD[String] containing "x,y" coordinates, filters out lines that either
+   * are missing x or y coordinate, or are empty.
+   * Corresponds to assignment TASK 2.
+   *
+   * @param rawData an RDD[String] containing the lines of the input raw data.
+   * @return an RDD[String] without the corrupted lines.
+   */
   private def filterOutLinesWithMissingValues(rawData: RDD[String]): RDD[String] = {
     rawData.filter(line =>
       !line.startsWith(",") &&
@@ -75,12 +124,14 @@ object Main {
     )
   }
 
-  //    ASSIGNMENT TASK 2
-  private def performDataCleaning(rawData: RDD[String]): RDD[(Double, Double)] = {
-    val cleanData = filterOutLinesWithMissingValues(rawData)
-    splitLinesToDoubleCoordinates(cleanData)
-  }
-
+  /**
+   * A utility function that, given an RDD[String] containing "x,y" coordinates, splits the coordinates
+   * to Double x, y values and constructs an RDD[(Double, Double)] with the result.
+   * Corresponds to assignment TASK 2.
+   *
+   * @param stringData the RDD[String] containing "x,y" coordinates to split.
+   * @return an RDD[(Double, Double)] with the split coordinates.
+   */
   private def splitLinesToDoubleCoordinates(stringData: RDD[String]): RDD[(Double, Double)] = {
     stringData.map(line => {
       val coordinates = line.split(",")
@@ -89,15 +140,30 @@ object Main {
   }
 
 
-  //    ASSIGNMENT TASK 3
+  /**
+   * A utility function that scales a given RDD containing coordinates to [0, 1] in both dimensions.
+   * Before calling the function, one should have executed computeMinMaxValues() function.
+   * Corresponds to assignment TASK 3.
+   *
+   * @param unscaled the RDD[(Double, Double)] containing coordinates to scale.
+   * @return an RDD[(Double, Double)] with the scaled values.
+   */
   private def performMinMaxScaling(unscaled: RDD[(Double, Double)]): RDD[(Double, Double)] = {
     unscaled.map(coordinates => {
       val x = coordinates._1
       val y = coordinates._2
+      // xMin, xMax, yMin, yMax are computed by computeMinMaxValues()
       ((x - xMin) / (xMax - xMin), (y - yMin) / (yMax - yMin))
     })
   }
 
+  /**
+   * A utility function that computes the minimum and maximum value for each dimension, given an RDD that contains
+   * coordinates. The computed values are stored in object's attributes xMin, xMax, yMin, yMax.
+   * Corresponds to assignment TASK 3.
+   *
+   * @param coordinates the RDD to compute min and max values on.
+   */
   private def computeMinMaxValues(coordinates: RDD[(Double, Double)]): Unit = {
     val x = coordinates.map(coordinates => coordinates._1)
     val y = coordinates.map(coordinates => coordinates._2)
@@ -107,6 +173,16 @@ object Main {
     yMax = y.max
   }
 
+  /**
+   * A utility function that transforms a given, scaled (x, y) pair to its original values
+   * (before MinMax scaling). The function uses the Object's attributes xMin, xMax, yMin, yMax
+   * to perform the inverse transformation.
+   * Corresponds to assignment TASK 3.
+   *
+   * @param xScaled the scaled value of x-coordinate.
+   * @param yScaled the scaled value of y-coordinate.
+   * @return a Vector containing the original (before scaling) coordinates values.
+   */
   private def getOriginalCoordinates(xScaled: Double, yScaled: Double): Vector[Double] = {
     Vector(
       xScaled * (xMax - xMin) + xMin,
@@ -115,7 +191,14 @@ object Main {
   }
 
 
-  //    ASSIGNMENT TASK 4
+  /**
+   * A utility function that creates a DataFrame from a RDD[(Double, Double)] containing coordinates.
+   * Corresponds to assignment TASK 4.
+   *
+   * @param coordinates the RDD to be converted to DataFrame.
+   * @param ss          the SparkSession object.
+   * @return a DataFrame containing the given RDD data with the appropriate schema.
+   */
   private def createDataFrameFromCoordinatesRDD(coordinates: RDD[(Double, Double)], ss: SparkSession): DataFrame = {
     val schema = StructType(
       StructField(name = "x", dataType = DoubleType, nullable = false) ::
@@ -190,10 +273,22 @@ object Main {
       .foreach(row => printf("(%.5f, %.5f)\n", row.getDouble(3), row.getDouble(4)))
   }
 
+  /**
+   * A utility function that writes K-means predictions to the specified directory as csv file(s).
+   *
+   * @param dataFrame the predictions DataFrame.
+   * @param outDir    the directory to write predictions in.
+   */
   private def writePredictions(dataFrame: DataFrame, outDir: String): Unit = {
     dataFrame.write.mode(SaveMode.Overwrite).csv(outDir)
   }
 
+  /**
+   * A utility function that writes the cluster centers found by K-means algorithm in csv format (header included).
+   *
+   * @param centers the cluster centers specified by K-means algorithm execution.
+   * @param outFile the file to write centers in.
+   */
   private def writeCenters(centers: Array[Vector[Double]], outFile: String): Unit = {
     writeToFile(outFile) { printer =>
       printer.println("x,y,clusterId")
@@ -201,6 +296,12 @@ object Main {
     }
   }
 
+  /**
+   * A utility function that writes content to the specified file.
+   *
+   * @param outFile the file to write in.
+   * @param op      the operation to perform.
+   */
   private def writeToFile(outFile: String)(op: java.io.PrintWriter => Unit): Unit = {
     val p = new PrintWriter(outFile)
     try {
